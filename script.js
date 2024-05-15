f6a34be9a5a70b0880af4749de1b57498b03eebe
Manuel26190 <manuelfaure26190@gmail.com>
2024-05-15 13:26:41 +0200

    save

--- script.js
+++ script.js
@@ -143,20 +143,18 @@ $('.dropdown-ul').hide()
 
 let toggleMenu = true
 
-console.log('toggle menu', toggleMenu)
-
 $('.toggle-menu-span').on('click', function (e) {
     if (toggleMenu) {
         $('.dropdown-ul').show()
         $('.main').css('marginTop', '130px')
         $('.main').css('transition', '700ms ease-in-out')
         toggleMenu = false
-        console.log('toggle menu', toggleMenu)
+        //console.log('toggle menu', toggleMenu)
     } else {
         $('.dropdown-ul').hide()
         $('.main').css('marginTop', '0px')
         toggleMenu = true
-        console.log('toggle menu', toggleMenu)
+        //console.log('toggle menu', toggleMenu)
     }
 })
 
@@ -166,5 +164,39 @@ $('.dropdown-ul a').on('click', function () {
     $('.main').css('marginTop', '0px')
     $('.dropdown-ul').hide()
     $('.toggle-menu-span').show()
-    console.log('toggle menu', toggleMenu)
+    //console.log('toggle menu', toggleMenu)
+})
+
+// Gestion de la NavBar disparaît / apparaît au mouvement / au click navbBar-link
+
+var prevScrollpos = scrollY
+window.onscroll = function () {
+    var currentScrollPos = scrollY
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById('header').style.top = '0'
+    } else {
+        document.getElementById('header').style.top = '-82px'
+    }
+    prevScrollpos = currentScrollPos
+}
+
+// Ajoute une hauteur de 5em
+const navBarLinks = document.querySelectorAll('.navbar-link')
+
+navBarLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault() // Empêcher le comportement de clic par défaut
+
+        const targetId = link.getAttribute('href').substring(1) // Récupérer l'identifiant de l'ancre
+        const targetElement = document.getElementById(targetId) // Récupérer l'élément cible
+
+        if (targetElement) {
+            const offset =
+                5 *
+                parseFloat(getComputedStyle(document.documentElement).fontSize) // Calculer le décalage en pixels
+            const offsetTop =
+                targetElement.getBoundingClientRect().top + scrollY - offset
+            window.scrollTo({ top: offsetTop, behavior: 'smooth' }) // Faire défiler la fenêtre jusqu'à l'ancre avec un décalage de 5em
+        }
+    })
 })
